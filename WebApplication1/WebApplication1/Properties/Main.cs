6d41d7886262db438909fbb9576f20fb5b25c83c
s34314 <s34314@pjwstk.edu.pl>
2026-04-16 23:44:43 +0200

    Main

---
+++ WebApplication1/WebApplication1/Properties/Main.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.OpenApi;
+using Microsoft.OpenApi.Modele;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Centrum Szkoleniowe API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Centrum Szkoleniowe API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
